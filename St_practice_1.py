88ca387d295763261170a27593fb323a85ef6244
Shivani-ML <163422544+Shivani-ML@users.noreply.github.com>
2024-04-28 18:43:23 +0530

    Add files via upload

---
+++ St_practice_1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title("My name is Shivani")
+st.markdown(""" # Best Brother
+## Dugu
+### Huggy
+:heart:<br> 
+:sunglasses:   
+**_ Mummy-Papa _**                    
+""",True )
+st.latex(r'''a+ ar+ar^2ar^3+\cdots=\sum_{k=0}^{n-1} ar^k''')
